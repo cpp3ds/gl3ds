89a4d8b1c876e53f0c726be8b9f802e02f6ab1f4
Cruel <machin3@gmail.com>
2015-06-05 17:01:49 -0400

    Fix export variables

--- .travis-deps.sh
+++ .travis-deps.sh
@@ -2,13 +2,11 @@
 set -e
 set -x
 
-export DEVKITPRO=/home/travis/devkitPro
-export DEVKITARM=${DEVKITPRO}/devkitARM
-export CTRULIB=${DEVKITPRO}/libctru
-
 # Build and install devkitARM + ctrulib
 wget http://sourceforge.net/projects/devkitpro/files/Automated%20Installer/devkitARMupdate.pl
 git clone https://github.com/smealum/ctrulib.git
 sudo perl devkitARMupdate.pl
-cd ctrulib/libctru && make ; cd -
-sudo cp -rf ctrulib/libctru/ ${DEVKITPRO}
+
+# Get latest ctrulib and overwrite bundled one
+# cd ctrulib/libctru && make ; cd -
+# sudo cp -rf ctrulib/libctru/ ${DEVKITPRO}
